ce3ed9311294890cc133380ed3ca8d8a5574ec26
s0lst1c3 <gabriel@solstice.me>
2017-03-26 22:15:17 -0400

    updating

--- config.py
+++ config.py
@@ -3,9 +3,11 @@ collector_db = './db-stash/collector.db'
 config_table = 'gw_config'
 scheduler_table = 'gw_jobs'
 
-iface='wlan1'
+iface='wlan0'
 collector_iface = 'wlan1'
 collector_tx_threshold = -69
+analyzer_t_threshold = 5 * 60
+analyzer_seq_delta = 500
 ap_start = '/opt/gamewarden/scripts/ap-start.sh'
 ap_stop = '/opt/gamewarden/scripts/ap-stop.sh'
 scheduler = '/opt/gamewarden/gw-scheduler'
